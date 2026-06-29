df022d0234e55aa1ad12fe1e60af811901b889b8
Lucas Zerpa <lenkagamine268@gmail.com>
2026-06-29 02:13:50 -0300

    Maquetación inicial del Home con buscador Bootstrap 5 y carrusel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home'; // Pag principal home
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // La ruta vacía (el inicio) carga el Home
+  { path: '**', redirectTo: '' } // Si alguien escribe una URL que no existe, lo manda al Home
+];
