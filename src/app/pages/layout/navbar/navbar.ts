eb96db753cf3ebd97cec933211bd7c8c02a731e2
Lucas Zerpa <lenkagamine268@gmail.com>
2026-06-29 19:06:29 -0300

    Refactorización de Navbar: Diseño de ejemplo por ahora, buscador dinámico con Router y arreglo del modo oscuro en Bootstrap 5

--- src/app/pages/layout/navbar/navbar.ts
+++ src/app/pages/layout/navbar/navbar.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,14 +10,24 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.css',
 })
 export class Navbar {
-  esModoOscuro: boolean = false;
+  esModoOscuro: boolean = true; // Variable para controlar el modo oscuro
+
+  // Inyectamos el Router de Angular acá para poder usarlo en el HTML
+  constructor(public router: Router) {}
+
+  ngOnInit() {  
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
+
+    // Anulamos el fondo estático que le habíamos puesto al index.html 
+    // para dejar que Bootstrap maneje los colores de fondo automáticamente
+    document.body.style.backgroundColor = '';
+  }
 
   ModoOscuro() {
     this.esModoOscuro = !this.esModoOscuro;
-  
-    if (this.esModoOscuro)
-      alert("MODO OSCURO ACTIVAO");
-    else
-      alert("MODO OSCURO DESACTIVADO");
+    
+    // Le decimos a Bootstrap que cambie toda la paleta de colores de la página
+    const tema = this.esModoOscuro ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-bs-theme', tema);
   }
 }
