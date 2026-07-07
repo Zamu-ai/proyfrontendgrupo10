b25d63b1f4f9e957f780ccc451147a7b7c72a409
Zamu-ai <uriel.b.zambrano@gmail.com>
2026-07-07 16:45:33 -0300

    arreglos

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -7,13 +7,16 @@ import { Observable } from 'rxjs';
 })
 export class AuthInterceptor implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token=localStorage.getItem('token');
-    if(token){
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization',`Bearer $(token)`)
+  //req es la peticion HTTP original
+  //next es el siguiente manejador en la cadenade interceptores
+  //retorna un Observable con el evento HTTP
+    const token=localStorage.getItem('token'); //obtengo el token del localStorage
+    if(token){ //si existe el token
+      const cloned = req.clone({ //clono la peticion original pq no se puede modificar directamente
+        headers: req.headers.set('Authorization',`Bearer $(token)`) //le agrego el header Authorization
       });
-      return next.handle(cloned);
+      return next.handle(cloned); //continuo con la peticion ya modificada
     }
-    return  next.handle(req)
+    return  next.handle(req) //si no habia token continuo con la peticion original
   }
 }
