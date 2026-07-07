da5e0c6e3e9383bc9d75586fac9c9282474c2dae
Zamu-ai <uriel.b.zambrano@gmail.com>
2026-07-07 16:00:41 -0300

    interceptores para validar token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+ 
+export const authInterceptor=(req:any, next: any)=>{
+  const token= localStorage.getItem('token');
+  if(token){
+    req=req.clone({
+      setHeaders:{
+        Authorization:`Bearer ${token}`
+      }
+    });
+  }
+  return next(req)
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
